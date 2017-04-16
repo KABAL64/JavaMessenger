d2628b1bf5f5ce2476527cb8318ff43b9c48227f
KABAL64 <KABAL64@yandex.ru>
2017-04-16 13:25:08 +0400

    Создание проекта. Первоначальная настройка

---
+++ client/src/main/java/ru/bfgsoft/client/Main.java
@@ -0,0 +1,16 @@
+package ru.bfgsoft.client;
+
+/**
+ * Приложение
+ */
+public class Main  {
+
+    /**
+     * Точка входа в приложение
+     * @param args Аргументы запуска
+     */
+    public static void main(String[] args) {
+    }
+
+
+}
